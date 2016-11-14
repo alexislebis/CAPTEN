4e917ceb2aa9ab6d9b4cabf1fe64ade030ef9de2
alexislebis <spher7@gmail.com>
2016-11-14 10:19:21 +0100

    serialization of vocab. Need to handle subclassof

---
+++ TRY/src/rdf-data-store/model/URI.js
@@ -0,0 +1,6 @@
+var TYPE_URI      = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+var DOMAIN_URI    = "http://www.w3.org/2000/01/rdf-schema#domain";
+var RANGE_URI     = "http://www.w3.org/2000/01/rdf-schema#range";
+var CLASS_URI     = "http://www.w3.org/2002/07/owl#Class";
+var PROPERTY_URI  = "http://www.w3.org/2002/07/owl#ObjectProperty";
+var UNION_URI     = "http://www.w3.org/2002/07/owl#unionOf";
