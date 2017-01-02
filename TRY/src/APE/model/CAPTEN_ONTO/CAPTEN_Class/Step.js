80e10ce2ff692d31792d76f8c97bb04e58b1cdcf
alexislebis <spher7@gmail.com>
2017-01-02 16:31:43 +0100

    refactoring step + debug/update workzone

--- TRY/src/APE/model/CAPTEN_ONTO/CAPTEN_Class/Step.js
+++ TRY/src/APE/model/CAPTEN_ONTO/CAPTEN_Class/Step.js
@@ -7,8 +7,8 @@ function Step() {
     this.settings = null; //At the difference to Indp.Op.specificSettings, it concerns all the settings needed for the step. not for the IndOp running.
     //The union of Indep.Op.specificSettings with this.settings constitute the whole --isConfiguredBy-->Setting of the ontology
     this.operator = null;
-    this.inputs = new RGTE(); //[RGTE]. It is actually see as a Union of the different inputs
-    this.outputs = new RGTE();
+    this.inputs = null; //[RGTE]. It is actually see as a Union of the different inputs
+    this.outputs = null;
     this.relationOrder = null; //Integer. Representing the place of this in the IAP.
 
     this.context = null; //TODO define CONTEXT notion
@@ -16,6 +16,208 @@ function Step() {
 
     this.creationDate = null;
     this.author = null; //using FOAF agent
+
+    //State of the Step (ouput generation) + notification
+    this.observers = [];
+    this.observersUnc = []; //Uncomp observer
+    this.isStateComputed = false; //If the output has been computed. MUST BE @ true when all the node expected are aligned with the input node
+    this.usedComputationInput = []; //State of the computation. Associative array. All the op.beh.inpu node must be aligned with one this.input.node
+    this.propAsyncBuild = new PropertyAsyncrhonousBuilder();
+      this.propAsyncBuild.registerObserverCallbackOnCompletion(this, this._callbackUsedConceptsInputComplete);
+      this.propAsyncBuild.registerObserverCallbackOnUncompletion(this, this._callbackUCIUncompletion);
 }
 
 Step.prototype = Object.create(CAPTENClass.prototype);
+
+Step.prototype = {
+
+  // === OBSERVATION
+    resetAllObservers: function()
+    {
+      this.observers = [];
+      this.observersUnc = [];
+    },
+    // === COMPUTATION
+    registerObserverCallbackOnOutputsComputation: function(objCallback, callback)
+    {
+      this.observers.push([objCallback,callback]);
+    },
+
+      // === NOTIFICATION
+      notifyOutputsComputation: function()
+      {
+        this.observers.forEach(function(e)
+        {
+          console.log(e);
+            if (typeof e[1] === "function") {
+              e[1].call(e[0]);//e[0] define the `this` context for e[1]
+            }
+        });
+      },
+
+      // === UNCOMPLETION
+      registerObserverCallbackOnUncompletion: function(objCallback, callback)
+      {
+        this.observersUnc.push([objCallback,callback]);
+      },
+
+        // === NOTIFICATION
+        notifyUncompletion: function()
+        {
+          this.observersUnc.forEach(function(e)
+          {
+            console.log(e);
+              if (typeof e[1] === "function") {
+                e[1].call(e[0]);//e[0] define the `this` context for e[1]
+              }
+          });
+        },
+    // ===
+
+  // === CALLBACK BEHAVIORS FROM PROPASYNC
+  _callbackUsedConceptsInputComplete: function()
+  {
+    this._computeOutput();
+  },
+
+  _computeOutput: function()
+  {
+      // console.log("COMPLETE");
+      var outObs = null;
+      if(this.outputs)
+        outObs = this.outputs.observers; //tmp
+
+      //TODO locate correct pattern regarding context of step
+      console.log("MATCH REGARDING CONTEXT");
+
+
+      this.outputs = this.inputs.merge(this.operator.behaviors.output);
+      this.outputs.resetObservers();
+
+      for(var i in this.propAsyncBuild.arrayToFill)
+      {
+        var fromID = this.outputs._getIdEquivalenceById("OLD_ID", this.propAsyncBuild.arrayToFill[i].from.id)[1];
+
+        for(var j in this.operator.behaviors.output.nodes)
+        {
+          var toID = this.outputs._getIdEquivalenceById("OLD_ID",this.operator.behaviors.output.nodes[j].id)[1];
+          this.outputs.addVisProperty(new Property(GENERATES_URI, 'generates', fromID, toID), 'to');
+        }
+      }
+
+      this.notifyOutputsComputation();
+  },
+
+  _callbackUCIUncompletion: function()
+  {
+    this.notifyUncompletion();
+  },
+  // ===
+
+  // === PUBLIC
+  changeOperator: function(op)
+  {
+      if (op == null)
+          return;
+
+      this.operator = op;
+
+      if (this.operator.behaviors != null)
+      {
+          this.propAsyncBuild.setSecondObject(this.operator.behaviors['input']);
+          this._updateUsedConcepts();
+      }
+
+      this.displayOperatorInputs = true;
+  },
+
+  changeRGTE: function(rgte)
+  {
+      if (rgte == null)
+          return;
+
+      this.inputs = rgte;
+
+      this.propAsyncBuild.setFirstObject(this.inputs);
+
+      this.displayRGTE = true;
+  },
+
+  bindRGTENOP: function(params)
+  {
+      if (this.isArrayFullyCompleted(this.usedComputationInput))
+      {
+          console.log("===WARNING===");
+          console.log("Currently, recomputing is not allowed. Thus, you could not change any bound between concepts once the configuration is over. Create a new step instead.");
+          console.log("=============");
+          return;
+      }
+
+      var obj = this.inputs.getNodeById(params.id);
+      if (obj == null)
+          obj = this.operator.behaviors['input'].getNodeById(params.id);
+
+      this.propAsyncBuild.bind(obj, USED_AS, 'usedAs');
+
+      return;
+  },
+
+  bindRGTEParams: function(params) {
+
+  },
+
+  /* _updateUsedConcepts reset the usedComputationInput and usedConceptsParams
+      arrays with the number of excepted concept
+      (number of rgte classes) of the operator input and parameters*/
+  _updateUsedConcepts: function()
+  {
+      if (this.operator == null || this.operator.behaviors == null)
+          return;
+
+      if (this.operator.behaviors['input'] != null)
+      {
+          this._updateUsedConceptsKeyed('input');
+          console.log(this.operator.behaviors['input'].getNodes().length);
+          this.propAsyncBuild.setArray(this.operator.behaviors['input'].getNodes().length);
+      }
+
+      if (this.operator.behaviors['params'] != null)
+      {
+          this._updateUsedConceptsKeyed('params');
+          console.log("TODO propAsyncBuild!!!!");
+      }
+
+      // this.propAsyncBuild.setArray(this.usedComputationInput);
+  },
+  _updateUsedConceptsKeyed: function(key)
+  {
+      var array = [];
+      var nodes = this.operator.behaviors[key].getNodes();
+
+      for (var i in nodes)
+      {
+          if (nodes[i].uri != null) //If uri exist, we use it. Otherwise, we use the value of node directly
+              array[nodes[i].id] = null;
+          else
+              array[nodes[i]] = null;
+      }
+
+      this.usedComputationInput = [];
+      this.usedComputationInput = array;
+  },
+
+  isArrayFullyCompleted: function(array) //Check only the 1st level
+      {
+          if (array == null)
+              return false;
+
+          for (var i in array)
+          {
+              if (array[i] == null)
+                  return false;
+          }
+
+          return true;
+      },
+
+};
