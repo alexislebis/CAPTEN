1f13c2417991c9eabdc2508eee1f29cdd82dc2df
alexislebis <spher7@gmail.com>
2017-11-17 11:08:45 +0100

    improving IRI flexibility with primitive

--- TRY/src/rdf-data-store/model/URI.js
+++ TRY/src/rdf-data-store/model/URI.js
@@ -9,43 +9,45 @@ var GENERATES_URI = "http://www.w3.or/ns/prov#generates";
 
 var CUSTOM_PREFIX_URI = "http://www.CAPTEN.org/SEED/ontologies/custom/";
 
+// === CUSTOM IRI PREFIX
+var CAPTEN_ONTO_IRI = "http://www.CAPTEN.org/SEED/ontologies/"; //TODO handle subblock of step matching a concept
 // === CUSTOM PROPERTIES
-var USED_AS = "http://www.CAPTEN.org/SEED/ontologies/usedAs";//TODO add it to the ontology OR locate it
-var INFLUENCES_URI = "http://www.CAPTEN.org/SEED/ontologies/influences";//TODO add it to the ontology OR locate it
-var FOLLOWED_BY_URI = "http://www.CAPTEN.org/SEED/ontologies/followedBy";
+var USED_AS = CAPTEN_ONTO_IRI+"usedAs";//TODO add it to the ontology OR locate it
+var INFLUENCES_URI = CAPTEN_ONTO_IRI+"influences";//TODO add it to the ontology OR locate it
+var FOLLOWED_BY_URI = CAPTEN_ONTO_IRI+"followedBy";
 var HAS_AUTHOR_URI = "http://xmlns.com/foaf/0.1/hasAgent";
-var IS_DESCRIBED_BY_URI = "http://www.CAPTEN.org/SEED/ontologies/isDescribedBy";
-var HAS_NAME_URI = "http://www.CAPTEN.org/SEED/ontologies/hasName";
-var HAS_CONTEXT_URI = "http://www.CAPTEN.org/SEED/ontologies/hasContext";
-var HAS_OBJECTIVE_URI = "http://www.CAPTEN.org/SEED/ontologies/hasObjective";
-var HAS_HYPOTHESIS_URI = "http://www.CAPTEN.org/SEED/ontologies/hasHypothesis";
-var HAS_DESCRIPTION_URI = "http://www.CAPTEN.org/SEED/ontologies/description";
-var HAS_TARGET_USER_URI = "http://www.CAPTEN.org/SEED/ontologies/targetUser";
-var COMES_FROM_URI = "http://www.CAPTEN.org/SEED/ontologies/comesFrom";//TODO Subclass of "http://www.w3.or/ns/prov#wasGeneratedBy";
-var IS_AUTHORED_BY = "http://www.CAPTEN.org/SEED/ontologies/isAuthoredBy";
+var IS_DESCRIBED_BY_URI = CAPTEN_ONTO_IRI+"isDescribedBy";
+var HAS_NAME_URI = CAPTEN_ONTO_IRI+"hasName";
+var HAS_CONTEXT_URI = CAPTEN_ONTO_IRI+"hasContext";
+var HAS_OBJECTIVE_URI = CAPTEN_ONTO_IRI+"hasObjective";
+var HAS_HYPOTHESIS_URI = CAPTEN_ONTO_IRI+"hasHypothesis";
+var HAS_DESCRIPTION_URI = CAPTEN_ONTO_IRI+"description";
+var HAS_TARGET_USER_URI = CAPTEN_ONTO_IRI+"targetUser";
+var COMES_FROM_URI = CAPTEN_ONTO_IRI+"comesFrom";//TODO Subclass of "http://www.w3.or/ns/prov#wasGeneratedBy";
+var IS_AUTHORED_BY = CAPTEN_ONTO_IRI+"isAuthoredBy";
 
 // === CUSTOM NODE
-var USE_CASE_URI = "http://www.CAPTEN.org/SEED/ontologies/useCase";
+var USE_CASE_URI = CAPTEN_ONTO_IRI+"useCase";
 var NAME_URI = "http://xmlns.com/foaf/0.1/name";
-var AUTHOR_URI = "http://www.CAPTEN.org/SEED/ontologies/Author";
-var ANALYSIS_URI = "http://www.CAPTEN.org/SEED/ontologies/NarratedAnalysisProcess";
-var OPERATOR_URI = "http://www.CAPTEN.org/SEED/ontologies/NarratedOperator";
-var OBJECTIVE_URI = "http://www.CAPTEN.org/SEED/ontologies/Objective";
-var HYPOTHESIS_URI = "http://www.CAPTEN.org/SEED/ontologies/Hypothesis";
-var STEP_URI = "http://www.CAPTEN.org/SEED/ontologies/Step";
-var TARGET_USER_URI = "http://www.CAPTEN.org/SEED/ontologies/TargetUser";
-var DESCRIPTION_URI = "http://www.CAPTEN.org/SEED/ontologies/Description";
+var AUTHOR_URI = CAPTEN_ONTO_IRI+"Author";
+var ANALYSIS_URI = CAPTEN_ONTO_IRI+"NarratedAnalysisProcess";
+var OPERATOR_URI = CAPTEN_ONTO_IRI+"NarratedOperator";
+var OBJECTIVE_URI = CAPTEN_ONTO_IRI+"Objective";
+var HYPOTHESIS_URI = CAPTEN_ONTO_IRI+"Hypothesis";
+var STEP_URI = CAPTEN_ONTO_IRI+"Step";
+var TARGET_USER_URI = CAPTEN_ONTO_IRI+"TargetUser";
+var DESCRIPTION_URI = CAPTEN_ONTO_IRI+"Description";
 
-var NARRATIVE_BLOCK_URI = "http://www.CAPTEN.org/SEED/ontologies/hasNarrativeBlock";//NOTE Outside the CAPTEN ontology
+var NARRATIVE_BLOCK_URI = CAPTEN_ONTO_IRI+"hasNarrativeBlock";//NOTE Outside the CAPTEN ontology
 
 
 // === NON INCLUDED
-var COMPOSITE_URI = "http://www.CAPTEN.org/SEED/ontologies/compositeElement";
-var EXTENDED_STRING_URI = "http://www.CAPTEN.org/SEED/ontologies/extendedString";
-var PARAMETER_PATTERN_URI = "http://www.CAPTEN.org/SEED/ontologies/parameterPattern";
-var PARAMETER_CONFIGURATION_URI = "http://www.CAPTEN.org/SEED/ontologies/parameterConfiguration";
-var RGTE_URI = "http://www.CAPTEN.org/SEED/ontologies/RGTE";
-var SuperRGTE_URI = "http://www.CAPTEN.org/SEED/ontologies/SUPERRGTE";
+var COMPOSITE_URI = CAPTEN_ONTO_IRI+"compositeElement";
+var EXTENDED_STRING_URI = CAPTEN_ONTO_IRI+"extendedString";
+var PARAMETER_PATTERN_URI = CAPTEN_ONTO_IRI+"parameterPattern";
+var PARAMETER_CONFIGURATION_URI = CAPTEN_ONTO_IRI+"parameterConfiguration";
+var RGTE_URI = CAPTEN_ONTO_IRI+"RGTE";
+var SuperRGTE_URI = CAPTEN_ONTO_IRI+"SUPERRGTE";
 
 
 
