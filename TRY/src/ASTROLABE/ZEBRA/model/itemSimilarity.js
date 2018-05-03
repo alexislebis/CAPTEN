9ca716103e3b33d688d05d6d0e01a43567673627
alexislebis <spher7@gmail.com>
2018-05-03 17:35:07 +0200

    namming + breaking rel into tr,t1,t2

---
+++ TRY/src/ASTROLABE/ZEBRA/model/itemSimilarity.js
@@ -0,0 +1,47 @@
+/**
+ * Hard correspondance between ID & Process name
+ */
+
+
+var ID_CORRESPONDANCE = [];
+
+// VERSION MATCHING : onto_A_T_boxes_behav_pp.owl
+ID_CORRESPONDANCE["49885"] = "Classification des comportements des utilisateurs dans le cadre d'un MOOC";
+ID_CORRESPONDANCE["52970"] = "Detection de patterns comportementaux des Joueur de Tamagocours";
+ID_CORRESPONDANCE["57422"] = "Nombre de points obtenus pour les Étudiant d'un Exercice de MOOC (duplicata)";
+ID_CORRESPONDANCE["48628"] = "Nombre de points obtenus pour les Étudiant d'un Exercice de MOOC";
+ID_CORRESPONDANCE["50188"] = "Classifier un  Apprenant du MOOC MOOCAZ de la plateforme COURSERA d'apres ses actions";
+ID_CORRESPONDANCE["50385"] = "Creer l'Indicateur permettant d'obtenir pour chaque Chapitre du MOOC MOOCAZ sur COURSERA le pourcentage et le nombre d'Apprenant de chaque categorie";
+ID_CORRESPONDANCE["52383"] = "Nombre de points obtenus pour les Étudiant d'un Exercice de MOOC (duplicata)";
+ID_CORRESPONDANCE["48310"] = "Analyse Activite QCM";
+ID_CORRESPONDANCE["48510"] = "Note Etudiant QCM";
+ID_CORRESPONDANCE["48722"] = "Nombre de Point total d'un Excercice de QCM";
+ID_CORRESPONDANCE["48820"] = "Notes Global Etudiants";
+ID_CORRESPONDANCE["48914"] = "Correlation Items Resultat Global";
+ID_CORRESPONDANCE["48988"] = "Reponses parfaites pour un Exercice de QCM";
+ID_CORRESPONDANCE["49716"] = "Traite les doublons d'Action";
+ID_CORRESPONDANCE["50659"] = "Analyse Activite QCM (duplicata)";
+ID_CORRESPONDANCE["50859"] = "Note Etudiant QCL (duplicata)";
+ID_CORRESPONDANCE["51071"] = "Nombre de Point total d'un Excercice de QCM (duplicata)";
+ID_CORRESPONDANCE["51169"] = "Notes Global Etudiants (duplicata)";
+ID_CORRESPONDANCE["51263"] = "Correlation Items Resultat Global (duplicata)";
+ID_CORRESPONDANCE["51337"] = "Reponses parfaites pour un Exercice de QCM (duplicata)";
+ID_CORRESPONDANCE["52065"] = "Analyse Activite QCM (duplicata)";
+ID_CORRESPONDANCE["52265"] = "Note Etudiant QCM (duplicata)";
+ID_CORRESPONDANCE["52477"] = "Nombre de Point total d'un Excercice de QCM (duplicata)";
+ID_CORRESPONDANCE["52575"] = "Notes Global Etudiants (duplicata)";
+ID_CORRESPONDANCE["52669"] = "Correlation Items Resultat Global (duplicata)";
+ID_CORRESPONDANCE["52743"] = "Reponses parfaites pour un Exercice de QCM (duplicata)";
+ID_CORRESPONDANCE["48428"] = "Detection de la justesse ou du caractere errone des Reponse";
+ID_CORRESPONDANCE["50777"] = "Detection de la justesse ou du caractere errone des Reponse (duplicata)";
+ID_CORRESPONDANCE["52183"] = "Detection de la justesse ou du caractere errone des Reponse (duplicata)";
+ID_CORRESPONDANCE["47948"] = "Mise en forme des variables d'un Joueur de Tamagocours pour definir son comportement";
+ID_CORRESPONDANCE["47771"] = "Codage d'un chat en fonction du contenu des echanges";
+ID_CORRESPONDANCE["49168"] = "Identifier les pratiques numeriques des Lyceen à partir de leurs actions en fonction du contexte";
+ID_CORRESPONDANCE["49398"] = "Identifier les pratiques numeriques des Lyceen du point de vue de leurs actions dans le temps";
+ID_CORRESPONDANCE["51517"] = "Identifier les pratiques numeriques des Lyceen à partir de leurs actions en fonction du contexte";
+ID_CORRESPONDANCE["51747"] = "Identifier les pratiques numeriques des Lyceen du point de vue de leurs actions dans le temps (duplicata)";
+ID_CORRESPONDANCE["52841"] = "Definir les comportements des Joueur au sein d'un Serious Game grace principalement aux interactions au sein d'un Chat et des reponses fournies";
+ID_CORRESPONDANCE["49086"] = "Purger les URL des entrees automatiques";
+ID_CORRESPONDANCE["51435"] = "Purger les URL des entrees automatiques (duplicata)";
+ID_CORRESPONDANCE["50977"] = "Nombre de points obtenus pour les Étudiant d'un Exercice de MOOC (duplicata)";
